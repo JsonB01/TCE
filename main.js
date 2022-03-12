fb13415ecce3785b5e20a6b5626e93a74a3ef7b1
JsonB01 <78325049+JsonB01@users.noreply.github.com>
2022-03-11 16:16:55 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function fonction() {
     const soleil = document.getElementById("home");
     soleil.classList.toggle("dark");
 }
+
+function appmenu() {
+    const chien = document.getElementById("menu");
+    chien.classList.toggle("show");
+}
